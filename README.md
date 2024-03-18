8823f4a83be6288d1dfb4f14d71c38c444343c52
Viktor Kustov <kustovvictor@gmail.com>
2024-03-18 06:57:44 +0300

    Add PrintArr

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 ### The result of execution of the code above:
 ```
+arr == [2, 3, 5, 7, 9, 12, 14]
 Result of comparison (lhs == 5) < (rhs == 7) is True
 Result of comparison (lhs == 5) < (rhs == 3) is False
 Result of comparison (lhs == 5) < (rhs == 5) is False
 Upper bound for value 5 is 7
+arr == [2, 3, 5, 7, 9, 12, 14]
 Result of comparison (lhs == 7) < (rhs == 5) is False
 Result of comparison (lhs == 3) < (rhs == 5) is True
 Result of comparison (lhs == 5) < (rhs == 5) is False
--- main.cpp
+++ main.cpp
@@ -20,23 +20,24 @@ class Compare {
     Ostream& out_;
 };
 
-template <typename T, typename Ostream = std::basic_ostream<char>,
+template <typename T, typename Container = std::vector<T>,
+          typename Ostream = std::basic_ostream<char>,
           typename Comp = Compare<T, Ostream>>
 class BinSearchChecher {
    public:
     BinSearchChecher() = default;
     BinSearchChecher(Ostream& out) : out_(out) {}
 
-    template <typename Container = std::vector<T>>
     void LowerBoundCheck(const Container& arr, const T& value) const {
+        PrintArr(arr);
         auto it = std::lower_bound(arr.begin(), arr.end(), value, comp_);
         out_ << "Lower bound for value " << value << " is "
              << (it != arr.end() ? std::to_string(*it) : "end iterator")
              << '\n';
     }
 
-    template <typename Container = std::vector<T>>
     void UpperBoundCheck(const Container& arr, const T& value) const {
+        PrintArr(arr);
         auto it = std::upper_bound(arr.begin(), arr.end(), value, comp_);
         out_ << "Upper bound for value " << value << " is "
              << (it != arr.end() ? std::to_string(*it) : "end iterator")
@@ -46,6 +47,20 @@ class BinSearchChecher {
    private:
     Ostream& out_ = std::cout;
     Comp comp_ = Comp(out_);
+
+    void PrintArr(const Container& arr) const {
+        out_ << "arr == [";
+        bool is_first = true;
+        for (const T& val : arr) {
+            if (is_first) {
+                is_first = false;
+            } else {
+                out_ << ", ";
+            }
+            out_ << val;
+        }
+        out_ << "]\n";
+    }
 };
 
 }  // namespace bin_search_checker
