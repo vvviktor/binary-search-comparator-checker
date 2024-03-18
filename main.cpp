d3a090375597e6adc9de9329ca618c7a05486e8e
Viktor Kustov <kustovvictor@gmail.com>
2024-03-18 04:26:47 +0300

    add Container template

--- main.cpp
+++ main.cpp
@@ -17,28 +17,30 @@ class Compare {
     Ostream& out_;
 };
 
-template <typename T, typename Ostream = std::basic_ostream<char>,
+template <typename T, typename Container = std::vector<T>,
+          typename Ostream = std::basic_ostream<char>,
           typename Comp = Compare<T, Ostream>>
 class BinSearchChecher {
    public:
+    BinSearchChecher() = default;
     BinSearchChecher(Ostream& out) : out_(out) {}
-    void LowerBoundCheck(const std::vector<T>& arr, const T& value) const {
+    void LowerBoundCheck(const Container& arr, const T& value) const {
         auto it = std::lower_bound(arr.begin(), arr.end(), value, comp_);
         out_ << "Lower bound for value " << value << " is " << *it << '\n';
     }
-    void UpperBoundCheck(const std::vector<T>& arr, const T& value) const {
+    void UpperBoundCheck(const Container& arr, const T& value) const {
         auto it = std::upper_bound(arr.begin(), arr.end(), value, comp_);
         out_ << "Upper bound for value " << value << " is " << *it << '\n';
     }
 
    private:
-    Ostream& out_;
+    Ostream& out_ = std::cout;
     Comp comp_ = Comp(out_);
 };
 
 int main(int, char**) {
     std::vector<int> arr{2, 3, 5, 7, 9, 12, 14};
-    BinSearchChecher<int> checker(std::cout);
+    BinSearchChecher<int> checker;
     checker.UpperBoundCheck(arr, 5);
     checker.LowerBoundCheck(arr, 5);
 }
